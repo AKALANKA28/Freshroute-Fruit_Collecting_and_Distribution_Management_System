495c8023d8b342af4f32eb4e2d6b4d6eeba87c23
Akalanka Dias <101487259+AKALANKA28@users.noreply.github.com>
2024-02-10 00:43:33 +0530

    first commit

---
+++ client/src/components/dashboard/sidebar/Sidebar.jsx
@@ -0,0 +1,135 @@
+import React from 'react'
+import './sidebar.css'
+import navList from './navItem';
+
+
+
+const Sidebar = () => {
+  return (
+    <div>
+      <aside id='sidebar' className='sidebar'>
+        <ul className="sidebar-nav" id='sidebar-nav'>
+
+            <li className='nav-item'>
+                <a className='nav-link' href='/'>
+                    <i className='bi bi-grid'></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li className='nav-item'>
+                <a className='nav-link collapsed' data-bs-target='#components-nav' data-bs-toggle='collapse' href='#'>
+                    <i className='bi bi-menu-button-wide'></i>
+                    <span>Documents</span>
+                    <i className='bi bi-chevron-down ms-auto'></i>
+                </a>
+
+                <ul id='components-nav' className='nav-content collapse' data-bs-parent='#sidebar-nav'>
+
+                    <li>
+                        <a href='#'>
+                            <i className='bi bi-circle'>
+                                <span>Customers</span>
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <i className='bi bi-circle'>
+                                <span>Customers</span>
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <i className='bi bi-circle'>
+                                <span>Customers</span>
+                            </i>
+                        </a>
+                    </li>
+
+                </ul>
+            </li>
+
+            <li className='nav-item'>
+                <a className='nav-link collapsed' data-bs-target='#forms-nav' data-bs-toggle='collapse' href='#'>
+                    <i className='bi bi-journal-text'></i>
+                    <span>Forms</span>
+                    <i className='bi bi-chevron-down ms-auto'></i>
+                </a>
+
+                <ul id='forms-nav' className='nav-content collapse' data-bs-parent='#sidebar-nav'>
+                    
+                    <li>
+                        <a href='#'>
+                            <i className='bi bi-circle'>
+                                <span>Application Form</span>
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <i className='bi bi-circle'>
+                                <span>Release Form</span>
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <i className='bi bi-circle'>
+                                <span>Cancellation Form</span>
+                            </i>
+                        </a>
+                    </li>
+
+                </ul>
+            </li>
+
+            <li className='nav-item'>
+                <a className='nav-link collapsed' data-bs-target='#tables-nav' data-bs-toggle='collapse' href='#'>
+                    <i className='bi bi-layout-text-window-reverse'></i>
+                    <span>Documents</span>
+                    <i className='bi bi-chevron-down ms-auto'></i>
+                </a>
+
+                <ul id='tables-nav' className='nav-content collapse' data-bs-parent='#sidebar-nav'>
+                    
+                    <li>
+                        <a href='#'>
+                            <i className='bi bi-circle'>
+                                <span>General Tables</span>
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <i className='bi bi-circle'>
+                                <span>Data Tables</span>
+                            </i>
+                        </a>
+                    </li>
+                   
+
+                </ul>
+            </li>
+
+            <li className='nav-heading'>Pages</li>
+            <div className='navList'>
+                {navList.map(nav => (
+                    <li className='nav-item' key={nav._id}>
+                    <a className='nav-link collapsed' href='#'>
+                        <i className={nav.icon}></i>
+                        <span>{nav.name}</span>
+                    </a>
+                </li>
+                ))}
+            </div>
+            
+
+        </ul>
+      </aside>
+    </div>
+  )
+}
+
+export default Sidebar
