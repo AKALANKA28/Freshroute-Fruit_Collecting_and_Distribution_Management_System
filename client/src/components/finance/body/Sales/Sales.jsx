4c9ba92b6af67c2884ce2b4ac7760a7509ac07ce
Akalanka Dias <101487259+AKALANKA28@users.noreply.github.com>
2024-03-18 11:17:39 +0530

    update akalanka

--- client/src/components/finance/body/Sales/Sales.jsx
+++ client/src/components/finance/body/Sales/Sales.jsx
@@ -1,16 +1,44 @@
-import React, {useEffect, useState} from 'react'
+import React,{useState, useEffect} from 'react'
+import CardFilter from '../CardFilter'
 import axios from 'axios';
 import SalesTable from "./SalesTables";
 
-import '../main.css'
+import './sales.css'
 
 
 const Sales = () => {
+  const [items, setItems] = useState([])
+    const [filter, setFilter] = useState('Today');
+    const handleFilterChange = filter => {
+        setFilter(filter)
+    };
+
+    const fetchData = () => {
+        fetch("http://localhost:8070/sales/")//add the backend link
+         .then(res => res.json())
+         .then(data => {
+            setItems(data);
+         })
+         .catch(e => console.log(e.message))
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
   return (
-    <div>
-      
+   
+    <div className='card recent-sales overflow-auto'>
+      <CardFilter filterChange={handleFilterChange} />
+      <div className="card-body">
+        <h5 className="card-title">
+          Recent Sales<span>| {filter}</span>
+        </h5>
+        <SalesTable items={items} />
+      </div>
     </div>
+   
   )
-}
+  }  
 
 export default Sales
