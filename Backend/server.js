7ecdbcb32bc912879ecd8e452bf9e3b41bd07876
Akalanka Dias <101487259+AKALANKA28@users.noreply.github.com>
2024-03-20 03:56:52 +0530

    updated expense table
    updated expense table with edit popup without editing, and other server.js changes with aseni's components

--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,6 @@ const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const cors = require("cors")
-
-
-
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const dotenv = require("dotenv");
 
 const app = express();
@@ -26,10 +19,7 @@ const vehicleRouter = require("./routes/transport/vehicleRoute.js");
 const processRouter = require("./routes/transport/processRoute.js");
 const coveringsRouter = require("./routes/transport/coveringsRoute.js");
 const router = require("./routes/farmers/farmerRoutes");
-
-const itemRouter = require("./routes/buyers/Bmanager")
-
-
+// const itemRouter = require("./routes/buyers/Bmanager")
 const EmployeeRouter = require("./routes/StaffManager/EmployeeRoute.js");
 const CalculateSalaryRouter = require("./routes/StaffManager/CalculateSalaryRoute.js");
 
@@ -56,14 +46,12 @@ app.use("/expense", expenseRouter);
 app.use("/cards", cardsRouter);
 app.use("/FruitType", FruitTypeRouter);
 app.use("/Salary", SalaryRouter);
-
 app.use('/schedule', scheduleRouter);
 app.use('/vehicle', vehicleRouter);
 app.use('/process', processRouter);
 app.use('/coverings', coveringsRouter);
 app.use('/Farmer', router);
-app.use(itemRouter);
-
+// app.use(itemRouter);
 app.use("/TransportFee", TransportFeeRouter);
 app.use("/schedule", scheduleRouter);
 app.use("/vehicle", vehicleRouter);
