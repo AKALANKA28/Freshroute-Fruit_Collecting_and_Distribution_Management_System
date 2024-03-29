ea16c9ad43b7bc4ca136608404a73482f52b06c6
HeshanChamika <it22194930@my.sliit.lk>
2024-03-29 22:17:27 +0530

    Update Login.jsx

--- client/src/components/LoginRegister/Login.jsx
+++ client/src/components/LoginRegister/Login.jsx
@@ -114,24 +114,17 @@ const login = () => {
              
         //     </button>
 
-            <span className='forgotPassword'>
-              Forgot your password? <a href='#'>Click Here</a>
-            </span>
-
         //     <span className='forgotPassword'>
         //       Forgot your password? <a href=''>Click Here</a>
         //     </span>
 
-
         //   </from>
-
 
          
         // </div>
     //             </div>
     //         </div>
     //     </div>
-
       
     // </div>
   )
