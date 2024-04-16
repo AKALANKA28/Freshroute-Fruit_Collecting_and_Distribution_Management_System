ccc8ed9211f8a89af620e641e9637a538a149145
Aseni Madhuka <it22211446@my.sliit.lk>
2024-04-16 10:10:12 +0530

    buyer- error fixed routes
    buyer- error fixed routes

--- Backend/routes/buyers/Bmanager.js
+++ Backend/routes/buyers/Bmanager.js
@@ -1,150 +1,63 @@
-const express = require("express")
+const express = require("express");
 const Request = require("../../models/Buyer/Bmanager");
 
 const router = express.Router();
 
-//save request
+// Save request
+router.post("/request/save", (req, res) => {
+    let newRequest = new Request(req.body);
 
-router.post("/request/save", (req, res) =>{
-  let newRequest = new Request(req.body);
-
-  newRequest.save((err) =>{
-    if(err){
-      return res.status(400).json({
-        error:err
-      })
-    }
-    return res.status(200).json({
-      success:"Request Save Successfully"
-    })
-  })
-})
-
-//get details
-
-router.get("/requests", (req, res) =>{
-    Request.find().exec((err, requests) =>{
-      if(err){
-        return res.status(400).json({
-          error:err
+    newRequest.save()
+        .then(() => {
+            res.status(200).json({ success: "Request Saved Successfully" });
         })
-      }
+        .catch((err) => {
+            res.status(400).json({ error: err });
+        });
+});
 
-      return res.status(200).json({
-        success:true,
-        existingRequest:requests
-      })
-    })
-})
+// Get all requests
+router.get("/requests", (req, res) => {
+    Request.find().exec()
+        .then((requests) => {
+            res.status(200).json({ success: true, existingRequest: requests });
+        })
+        .catch((err) => {
+            res.status(400).json({ error: err });
+        });
+});
 
-//update
+// Update request
+router.put("/request/update/:id", (req, res) => {
+    Request.findByIdAndUpdate(req.params.id, { $set: req.body }).exec()
+        .then(() => {
+            res.status(200).json({ success: "Update Successful" });
+        })
+        .catch((err) => {
+            res.status(400).json({ error: err });
+        });
+});
 
-router.put("/request/update/:id", (req, res) =>{
-    Request.findByIdAndUpdate(
-     req.params.id,
-     {
-       $set:req.body
-     },
-     (err, request) =>{
-       if(err){
-         return res.status(400).json({error:err})
-       }
- 
-       return res.status(200).json({
-         success:"update successfully"
-       })
-     }
-    )
- })
+// Delete request
+router.delete("/request/delete/:id", (req, res) => {
+    Request.findByIdAndRemove(req.params.id).exec()
+        .then((deleteRequest) => {
+            res.status(200).json({ message: "Delete Successful", deleteRequest });
+        })
+        .catch((err) => {
+            res.status(400).json({ message: "Delete Unsuccessful", error: err });
+        });
+});
 
- //delete
+// Get a specific request
+router.get("/request/:id", (req, res) => {
+    Request.findById(req.params.id).exec()
+        .then((request) => {
+            res.status(200).json({ success: true, request });
+        })
+        .catch((err) => {
+            res.status(400).json({ success: false, error: err });
+        });
+});
 
-router.delete("/request/delete/:id", (req, res) =>{
-    Request.findByIdAndRemove(req.params.id).exec((err, deleteRequest) =>{
-      if(err) return res.status(400).json({
-        message:"Delete Unsuccessfull", err
-      })
-  
-      return res.json({
-        message:"Delete Susccessfull" , deleteRequest
-      })
-    })
-  })
-
-  //get a specific request
-
-router.get("/request/:id",(req, res) =>{
-    let requestId = req.params.id;
-  
-    Request.findById(requestId,(err, request) =>{
-      if(err){
-        return res.status(400).json({success:fails, err})
-      }
-  
-      return res.status(200).json({
-        success:true,
-        request
-      })
-    })
-  })
-  
-  module.exports = router 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-// // const router = express.Router();
-// // const BuyerCtrl = require("../../controllers/Buyer/BuyerCtrl")
-
-// // const express=require("express");
-
-// // router.post("/request/save", BuyerCtrl.newRequest);
-// // router.get("/requests", BuyerCtrl.Request.find);
-// // router.put("/request/update/:id", BuyerCtrl.Request.findByIdAndUpdate);
-// // router.delete("/request/delete/:id", BuyerCtrl.Request.findByIdAndRemove);
-// // router.get("/request/:id", BuyerCtrl.Request.findById);
-
-// // module.exports = router
-
-
-
-// // const router = express.Router();
-// // const BuyerCtrl = require("../../controllers/Buyer/BuyerCtrl")
-// // const { saveRequest, getRequests, updateRequest, deleteRequest,getRequestById } = require("../../controllers/Buyer/BuyerCtrl");
-
-// // const express=require("express");
-
-// // router.post("/request/save", saveRequest);
-// // router.get("/requests", getRequests);
-// // router.put("/request/update/:id", updateRequest);
-// // router.delete("/request/delete/:id", deleteRequest);
-// // router.get("/request/:id", getRequestById);
-
-// // module.exports = router
-
-// // const express = require("express");
-// // const { saveRequest, getRequests, updateRequest, deleteRequest, getRequestById } = require("../../controllers/Buyer/BuyerCtrl");
-
-
-// // const router = express.Router();
-
-// // router.post("/request/save", saveRequest);
-// // router.get("/requests", getRequests);
-// // router.put("/request/update/:id",updateRequest);
-// // router.delete("/request/delete/:id",deleteRequest);
-// // router.get("/request/:id",getRequestById);
-
-// // module.exports = router;
+module.exports = router;
