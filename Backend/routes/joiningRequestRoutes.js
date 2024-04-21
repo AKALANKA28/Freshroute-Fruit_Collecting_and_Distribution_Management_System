960a0991495aadf5c9d122e490e5bdc64ccf44ec
HeshanChamika <it22194930@my.sliit.lk>
2024-04-21 15:37:16 +0530

    Update joiningRequestRoutes.js

--- Backend/routes/joiningRequestRoutes.js
+++ Backend/routes/joiningRequestRoutes.js
@@ -1,17 +1,3 @@
-// //C:\Users\User\OneDrive\Desktop\ITP\MERN_Project\Backend\routes\joiningRequestRoutes.js
-// const router = require("express").Router();
-// const joiningRequestController = require("../controllers/joiningRequestController");
-
-// router.post("/add", joiningRequestController.addJoiningRequest);
-// router.get("/", joiningRequestController.getAllJoiningRequests);
-// router.get("/get/:id", joiningRequestController.getOneJoiningRequest);
-// router.delete("/delete/:id", joiningRequestController.deleteJoiningRequest);
-// router.put("/update/:id", joiningRequestController.updateJoiningRequest);
-// router.put("/accept/:id", joiningRequestController.acceptJoiningRequest);
-// router.put("/decline/:id", joiningRequestController.declineJoiningRequest);
-
-// module.exports = router;
-
 const router = require("express").Router();
 const joiningRequestController = require("../controllers/joiningRequestController");
 
