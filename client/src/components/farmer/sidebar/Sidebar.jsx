fd3683c10012d3291632c379e021d44bf26a5eb5
HeshanChamika <it22194930@my.sliit.lk>
2024-04-23 11:28:21 +0530

    sidebar changes in farmer

--- client/src/components/farmer/sidebar/Sidebar.jsx
+++ client/src/components/farmer/sidebar/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import navList from './navItem';
 import { Modal, Button } from 'react-bootstrap';
 import PredictionForm from '../body/PredictionDetails/PredictionForm'
 
@@ -67,46 +66,15 @@ const Sidebar = () => {
                 </ul>
             </li>
 
-            <li className='nav-item'>
-                <a className='nav-link collapsed' data-bs-target='#tables-nav' data-bs-toggle='collapse' href='#'>
-                    <i className='bi bi-layout-text-window-reverse'></i>
-                    <span>Documents</span>
-                    <i className='bi bi-chevron-down ms-auto'></i>
-                </a>
-
-                <ul id='tables-nav' className='nav-content collapse' data-bs-parent='#sidebar-nav'>
-                    
-                    <li>
-                        <a href='#'>
-                            <i className='bi bi-circle'>
-                                <span>General Tables</span>
-                            </i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#'>
-                            <i className='bi bi-circle'>
-                                <span>Data Tables</span>
-                            </i>
-                        </a>
-                    </li>
-                   
-
-                </ul>
-            </li>
-
-            <li className='nav-heading'>Pages</li>
-            <div className='navList'>
-                {navList.map(nav => (
-                    <li className='nav-item' key={nav._id}>
-                    <a className='nav-link collapsed' href ={'/login'}>
-                        <i className={nav.icon}></i>
-                        <span>{nav.name}</span>
-                    </a>
-                </li>
-                ))}
-            </div>
             
+
+            <hr></hr>
+            <li className='nav-item'>
+                <a className='nav-link' href='#'>
+                    <i className='bi bi-grid'></i>
+                    <span>Logout</span>
+                </a>
+            </li>
 
         </ul>
 
