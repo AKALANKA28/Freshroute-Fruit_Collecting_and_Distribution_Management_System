5cad546ad1893c8d8606c1db0ae0edf01fa0be56
HeshanChamika <it22194930@my.sliit.lk>
2024-04-23 16:31:39 +0530

    Update SupplierReport.jsx

--- client/src/components/supplierManagerDashboard/body/SupplierDetails/SupplierReport.jsx
+++ client/src/components/supplierManagerDashboard/body/SupplierDetails/SupplierReport.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PDFViewer, Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
+import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
 import './farmers.css';
 
 const styles = StyleSheet.create({
