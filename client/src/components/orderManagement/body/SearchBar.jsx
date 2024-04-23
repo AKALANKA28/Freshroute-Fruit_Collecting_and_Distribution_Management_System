53605129fa5e17f56968bdedcd13bd762cee89eb
SasankaWelikanna <sasankatw@gmail.com>
2024-04-23 09:15:37 +0530

    oder manager filter model changes

--- client/src/components/orderManagement/body/SearchBar.jsx
+++ client/src/components/orderManagement/body/SearchBar.jsx
@@ -5,14 +5,17 @@ const SearchBar = ({enableFilterType, filterColumns, handleSearch, clearInputFie
 
     const [inputValue, setInputValue] = useState("");
     const [filterType, setFilterType] = useState("");
+    const [filterDataType, setFilterDataType] = useState("text")
     useEffect(()=>{
         setFilterType(filterColumns[0].tag);
+        setFilterDataType(filterColumns[0].type? filterColumns[0].type: 'text');
         setInputValue("")
     },[])
 
     useEffect(() => {
         setInputValue("");
         setFilterType(filterColumns[0].tag)
+        setFilterType(filterColumns[0].type? filterColumns[0].type: 'text')
     }, [clearInputField]);
     const handleOnSubmit = (event) => {
         event.preventDefault();
@@ -29,23 +32,30 @@ const SearchBar = ({enableFilterType, filterColumns, handleSearch, clearInputFie
     const handleFilterTypeChange = (event) => {
         const{ value } = event.target;
         setFilterType(value);
+        filterColumns.map((filter) => {
+            if (filter.tag === value) {
+                setFilterDataType(filter.type? filter.type : 'text');
+            }
+        })
     }
   return (
     <div>
         <div className='search-bar search-bar-width'>
             <form className='search-form w-100 d-flex flex-row justify-content-end pb-1' onSubmit={handleOnSubmit}>
-                <div className="m-0 w-25" ></div>
+                <div className="m-0 w-25"></div>
                 {enableFilterType && filterColumns &&
-                    <select className="w-25 m-0 form-select rounded-4 text-secondary" name="filterType" id="dropdown" style={{maxWidth:'200px'}} onChange={handleFilterTypeChange}
-                    value={filterType}>
+                    <select className="w-25 m-0 form-select rounded-4 text-secondary" name="filterType" id="dropdown"
+                            style={{maxWidth: '200px'}} onChange={handleFilterTypeChange}
+                            value={filterType}>
                         {filterColumns.map((value, index) => (
                             <option key={index} value={value.tag}>{value.name}</option>
                         ))}
                     </select>
                 }
 
-                <div className="m-0 w-50" style={{maxWidth:'300px'}}>
-                    <input type='text' name='filterValue' placeholder='Search' title='Enter search keyword' onChange={handleInputChange} value={inputValue}/>
+                <div className="m-0 w-50" style={{maxWidth: '300px'}}>
+                    <input type={filterDataType} name='filterValue' placeholder='Search' title='Enter search keyword'
+                           onChange={handleInputChange} value={inputValue}/>
                     <button type='submit' title='Search'>
                         <i className='bi bi-search'>
                         </i>
