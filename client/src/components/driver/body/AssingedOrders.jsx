1805814da336891b4669e6ba88ebab6a76271a99
Akalanka Dias <101487259+AKALANKA28@users.noreply.github.com>
2024-05-08 06:35:35 +0530

    update

--- client/src/components/driver/body/AssingedOrders.jsx
+++ client/src/components/driver/body/AssingedOrders.jsx
@@ -1,45 +1,80 @@
-import React, {useState, useEffect} from 'react'
-import CardFilter from './CardFilter'
-import AssingedOrdersItem from './AssingedOrdersItem'
-
+import React, { useState, useEffect } from "react";
+import CardFilter from "./CardFilter";
+import AssingedOrdersItem from "./AssingedOrdersItem";
 
 const AssingedOrders = () => {
+  const [items, setItems] = useState([]);
 
-    const [items, setItems] = useState([])
-   
+  const fetchData = () => {
+    fetch("http://localhost:8070/schedule/")
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+      })
+      .catch((e) => console.log(e.message));
+  };
 
-    const fetchData = () => {
-        fetch("http://localhost:8070/schedule/")
-         .then(res => res.json())
-         .then(data => {
-            setItems(data);
-         })
-         .catch(e => console.log(e.message))
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, [])
- 
+  const handleStatus = (status) => {
+    switch (status) {
+      case "Accept":
+        return "success";
+        break;
+      case "Warning":
+        return "warning";
+        break;
+      case "Rejected":
+        return "danger";
+        break;
+      default:
+        return "success";
+    }
+  };
+
   return (
-    <div className='card'>
+    <div className="card">
       <div className="card-body">
-        <h5 className="card-title">
-          Assigned Orders
-        </h5>
+        <h5 className="card-title">Assigned Orders</h5>
         <div className="activity text-dark">
-      
+          <div>
+            <table className="table table-bordeless datatable">
+              <thead className="table-light">
+                <tr>
+                  <th className="col" d>
+                    Location
+                  </th>
+                  <th className="col" d>
+                    Date
+                  </th>
+                  <th className="col" d>
+                    Action{" "}
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Thirappane, Anurdhapura</td>
+                  <td>28/05/24</td>
+                  <td>
+                    <span className="bg-success rounded" style={{padding: "6px 12px", color: "White"}}>Accept</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
-          {items &&
+          {/* {items &&
           items.lenght > 0 &&
           items.map(item => (
             <AssingedOrdersItem key={item._id} item={item}/>
-            ))}
-      </div>
-       
+            ))} */}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AssingedOrders
+export default AssingedOrders;
--- client/src/components/driver/body/Transactions.jsx
+++ client/src/components/driver/body/Transactions.jsx
@@ -1,51 +1,62 @@
-import React, { useEffect, useState} from 'react'
-import CardFilter from './CardFilter';
-import NewsPostItem from './TransactionsPostItem';
+import React, { useEffect, useState } from "react";
+import CardFilter from "./CardFilter";
+import NewsPostItem from "./TransactionsPostItem";
+import img from "../../../assests/truck.png";
 
+import "./main.css";
 const Transactions = () => {
-
-    const [news, setNews] = useState([])
-    const [filter, setFilter] = useState('Today');
-    const handleFilterChange = filter => {
-        setFilter(filter)
-    };
+  const [news, setNews] = useState([]);
 
 
-    const fetchData = () => {
-        fetch("")
-         .then(res => res.json())
-         .then(data => {
-            setNews(data);
-         })
-         .catch(e => console.log(e.message))
-    };
+  const fetchData = () => {
+    fetch("")
+      .then((res) => res.json())
+      .then((data) => {
+        setNews(data);
+      })
+      .catch((e) => console.log(e.message));
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, [])
-
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
-      <div className='card transaction-card'>
-        <CardFilter filterChange={handleFilterChange} />
-      <div className="card-body pb-0">
-        <h5 className="card-title">
-           Transactions <span>| {filter}</span>
-        </h5>
+      <div className="card transaction-card">
+        <div className="card-body pb-0">
+          <h5 className="card-title">
+            Assigned Vehicle <span></span>
+          </h5>
 
-        <div className='news'>
+          <div className="vehicle-details">
+            <img src={img} alt="" className="vehicle-img" />
+            <div className="vehicle-content">
+              <p className="" style={{ fontSize: "14px", color: "black" }}>
+               <span className="text-bold"> Vehicle No:</span><span className="me-3"> CP4592</span>
+                <span className="text-bold"> Engine:</span><span> 5.2L - Diesel</span>
+                <span className="text-bold"> Transmission:</span><span> Automatic Transmision</span>  <br/>
+                <span className="text-bold"> Tank Capacity:</span><span> 30 Galloon</span>  <br/> 
+                <span className="text-bold"> Payload:</span><span> 10,000 lbs </span>
+
+               </p>
+              <p className="" style={{ fontSize: "14px", color: "black" }}>
+             
+              </p>
+            </div>
+          </div>
+
+          {/* <div className='news'>
             {
                 news &&
                 news.length > 0 &&
                 news.map(item => <NewsPostItem key={item._id} item={item}/>)
             }
+        </div> */}
         </div>
-        
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Transactions
+export default Transactions;
