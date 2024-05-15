b74e61f847687ea756767aaf61ea08dafc22ae2a
Nadil Senruka <it22144430@my.sliit.lk>
2024-05-15 09:10:15 +0530

    report signature
    report signature

--- client/src/components/StaffManager/body/CalculateSalary/CalculateSalaryReport.jsx
+++ client/src/components/StaffManager/body/CalculateSalary/CalculateSalaryReport.jsx
@@ -94,6 +94,29 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
+  signatureContainerCenter: {
+    marginTop: 40, 
+    flexDirection: "column", 
+    alignItems: "center", 
+    marginRight: 20, 
+  },
+  signatureContainer: {
+    marginTop: 40,
+    flexDirection: "column", 
+    alignItems: "flex-end", 
+    marginRight: 20, 
+  },
+  signatureLine: {
+    width: 200,
+    borderBottomWidth: 1,
+    borderBottomColor: "#333333",
+    marginTop: 8,
+    borderStyle: "dashed",
+  },
+  signatureText: {
+    fontSize: 10,
+    color: "#333333",
+  },
 });
 
 const Footer = () => (
@@ -141,6 +164,12 @@ const EmployeeReport = ({ dataList }) => {
               </View>
            ))}
            </View>
+           <View style={styles.signatureContainer}>
+          <View style={styles.signatureContainerCenter}>
+            <View style={styles.signatureLine} />
+           <Text style={styles.signatureText}>Signature</Text>
+        </View>
+        </View>
          </View>
          <Footer />
        </Page>
